056b56790014c3b03f23db82133cdaee59cae25b
VictorFernandezF <victorfernandezflores2@gmail.com>
2023-12-14 11:01:00 +0100

    Form

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(){
     const res = await fetch('http://localhost:3001/items',{
         headers : {'Content-Type' : 'application/json'},
-        next: { revalidate: 1 },
+        next: { revalidate: 0 },
     });
     const data = await res.json();
     return NextResponse.json({ data });
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
+'use client'
 
-export default function Home() {
-/*   async function submit(e : FormEvent<HTMLFormElement>){
+import { FormEvent } from "react";
+
+export default async function Home() {
+  async function submit(e : FormEvent<HTMLFormElement>){
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const response = await fetch("/api/items", {method: 'POST', body: formData});
   }
-  const data = await fetch('http://localhost:3000/api/items')
-  const res = await data.json();
- */
+
 return (
   <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <form /* onSubmit={submit} */>
+    <form  onSubmit={submit} >
       <input type="text" name="name" /><br/><br/>
       <input type="text" name="comment" /><br/><br/>
       <input type="submit" name="ADD" value="Add" />
